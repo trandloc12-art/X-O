8a6c41bae3c9c4830046d13c1def86860316ead3
locpro1504-svg <locpro1504@gmail.com>
2026-08-18 21:31:31 +0700

    update table

---
+++ function/functions.h
@@ -0,0 +1,5 @@
+#ifdef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void drawTable(const Table& table); // Function to draw the table on the console
+void drawXO(const Table& table, int row, int col); // Function to draw the X and O symbols on the console based on the table data
