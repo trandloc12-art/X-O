6b9c7391f926a40896a2832ab57a28c3259680b1
locpro1504-svg <locpro1504@gmail.com>
2026-08-19 09:53:07 +0700

    fix bug

--- dto/table.h
+++ dto/table.h
@@ -1,13 +1,15 @@
 #ifndef TABLE_H
 #define TABLE_H
 
+#include <vector>
+
 class Table {
 private:
     std::vector<std::vector<int>> data;
     int rows;
     int cols;
 public:
-    Table(int r, int c) : rows(r), cols(c);
+    Table(int r, int c) ;// Constructor to initialize rows, cols and create data of size r x c, all values set to 0
 
     void setValue(int r, int c, int value);// Method to set a value in the table at specified row and column
 
@@ -17,4 +19,6 @@ public:
     int getCols() const;// Method to get the number of columns in the table
     const std::vector<std::vector<int>>& getTable() const;// Method to get the entire table data
 };
+
+#endif // TABLE_H
 
--- function/functions.h
+++ function/functions.h
@@ -1,5 +1,9 @@
-#ifdef FUNCTIONS_H
+#ifndef FUNCTIONS_H
 #define FUNCTIONS_H
+#include "raylib.h"
 
 void drawTable(const Table& table); // Function to draw the table on the console
 void drawXO(const Table& table, int row, int col); // Function to draw the X and O symbols on the console based on the table data
+bool isDrawXO(const Table& table, int row, int col); // Function to check if blank space is available for drawing X or O at the specified row and column
+bool isWinXO(const Table& table, char player); // Function to check if the specified player has won the game based on the current state of the table
+#endif // FUNCTIONS_H
