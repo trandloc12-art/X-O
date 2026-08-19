039839f0e3158ca15e8c0befc69e96a84b0bc09b
locpro1504-svg <locpro1504@gmail.com>
2026-08-19 15:45:02 +0700

    update gitignore

--- build/raylib_build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/libs/raylib-6.0
-# Build directory: C:/Code cua loc/TaiLieuNgienCuuGame/New folder/X-O/build/raylib_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
