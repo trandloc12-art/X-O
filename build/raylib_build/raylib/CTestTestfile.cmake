c077e585bbe603a6a9eb69fec2a302416a337861
locpro1504-svg <locpro1504@gmail.com>
2026-08-19 13:28:01 +0700

    ignore build folder

--- build/raylib_build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/libs/raylib-6.0/src
-# Build directory: C:/Code cua loc/TaiLieuNgienCuuGame/New folder/X-O/build/raylib_build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
